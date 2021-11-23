11ff9b003f108fbf9648b38541d35eb7024d1117
Anikay <aniketshinde99999@gmail>
2021-11-23 15:24:30 +0530

    created the project

---
+++ src/GitClass.java
@@ -0,0 +1,14 @@
+
+public class GitClass {
+
+	public static void main(String[] args) {
+		System.out.println("Namaste Guys :)");
+		System.out.println("This is First Git Project!!");
+		System.out.println("Done by the Lead.");
+	}
+
+}
+/*
+	
+
+*/
